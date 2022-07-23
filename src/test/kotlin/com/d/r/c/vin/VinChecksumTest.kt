5f0ef33c0bb1d11aba061bbd6ea4916d6afba132
c dorman <crdorman@gmail.com>
2022-07-22 22:31:34 -0400

    rename test file

--- src/test/kotlin/com/d/r/c/vin/VinChecksumTest.kt
+++ src/test/kotlin/com/d/r/c/vin/VinChecksumTest.kt
@@ -3,7 +3,7 @@ package com.d.r.c.vin
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
-internal class VinValidatorKtTest {
+internal class VinChecksumTest {
     @Test
     internal fun `valid checksum returns true`() {
         assertTrue(checksum("1FTDX1868WKA10140"))
